305492da1768f8c4380f5e225c5b98d85b64d3ae
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2024-07-30 15:27:56 -0400

    Delete Carro-e-modelo/Carros directory

--- Carro-e-modelo/Carros/src/main/java/br/edu/ifms/Carros/repository/CarrosRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifms.Carros.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifms.Carros.modelo.Carros;
-
-public interface CarrosRepository  extends JpaRepository <Carros, Long>  {
-}
--- Carro-e-modelo/Carros/src/main/java/br/edu/ifms/Carros/repository/ModeloRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifms.Carros.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifms.Carros.modelo.Modelo;
-
-public interface ModeloRepository extends JpaRepository<Modelo, Long> {
-}
